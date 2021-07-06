f33118a5f9e29b261b0465f77a50c0b168ae8c45
Sarath-97 <sarath559709@gmail.com>
2021-07-06 15:18:29 +0100

    navbar, footer done

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="container-fluid bg-primary text-color-light">
+      <p className="d-flex justify-content-center">This is my footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
---
+++ src/Welcome.jsx
@@ -0,0 +1,24 @@
+import { Navbar, Nav } from "react-bootstrap";
+
+const Jumbotron = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">BookStore</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Home</Nav.Link>
+          <Nav.Link href="#pricing">About</Nav.Link>
+          <Nav.Link href="#pricing">Browse</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Jumbotron;
